482e87700096853fe80a69937dbddc40699427ec
Aleksey-Dr <aprel181818@gmail.com>
2023-03-29 16:03:18 +0300

    The solution of the task 1
    The solution of the task 1
    JS:
    Find elements (buttons)
    Add listeners to elements (buttons)
    Create a variable for interval id
    Add callbacks for listeners
    Blocking button startBtn after click
    Unblocking button startBtn after click to stopBtn
    Add function for generate a random color
    HTML:
    Add inline-styles

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+// console.log('Hello!');
+// Find elements (buttons)
+const backgroundBody = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+// Add listeners to elements (buttons)
+startBtn.addEventListener('click', startChangeColor);
+stopBtn.addEventListener('click', stopChangeColor);
+
+// Create a variable for interval id
+let switcherColor = null;
+
+// Add callbacks for listeners
+function startChangeColor() {
+    switcherColor = setInterval(() => {
+        // console.log('Change color');
+        backgroundBody.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    // Blocking button startBtn after click
+    startBtn.disabled = true;
+};
+
+function stopChangeColor() {
+    // console.log('Stop change color');
+    clearInterval(switcherColor);
+    // Unblocking button startBtn after click to stopBtn
+    startBtn.disabled = false;
+}
+
+// Add function for generate a random color
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
